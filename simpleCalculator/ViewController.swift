b45df01fac04bae47868689863083c0144d1b2fc
JO <aceu@JOs-Mac.local>
2018-05-24 22:38:44 +1000

    Very first commit

--- simpleCalculator/ViewController.swift
+++ simpleCalculator/ViewController.swift
@@ -19,7 +19,46 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    
+    @IBOutlet weak var X: UITextField!
+    
+    @IBOutlet weak var Y: UITextField!
+    
+    @IBOutlet weak var Result: UILabel!
+    @IBOutlet weak var History: UITextView!
+    
+    
+    @IBAction func calcOps(_ sender: UIButton) {
+        var value : Double = 0;
+        switch sender.tag {
+        case 0:
+            value = Double(X.text!)! + Double(Y.text!)!
+        case 1:
+            value = Double(X.text!)! - Double(Y.text!)!
+        case 2:
+            value = Double(X.text!)! * Double(Y.text!)!
+        case 3:
+            value = Double(X.text!)! / Double(Y.text!)!
+        default:
+            print ("Error")
+        }
+        switch sender.tag {
+        case 0:
+            History.text = "\(History.text!) \n \(X.text!) + \(Y.text!) = \(value)"
+        case 1:
+            History.text = "\(History.text!) \n \(X.text!) - \(Y.text!) = \(value)"
+        case 2:
+            History.text = "\(History.text!) \n \(X.text!) * \(Y.text!) = \(value)"
+        case 3:
+            History.text = "\(History.text!) \n \(X.text!) / \(Y.text!) = \(value)"
+        default:
+            print("Error!")
+        }
+        
+        Result.text = "Result:" + "\n \(value)"
+    }
+    
 }
 
